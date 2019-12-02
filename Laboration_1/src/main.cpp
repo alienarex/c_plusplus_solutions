3f3e5a1cb1c612701499b634d219d2edb6b6569c
anbj1001 <anbj1001@student.miun.se>
2019-12-02 22:14:55 +0100

    Do-while loop to control input works

--- Laboration_1/src/main.cpp
+++ Laboration_1/src/main.cpp
@@ -8,10 +8,11 @@ using namespace std;
 bool isCorrectLength(int inputNumber) {
     // Source: https://stackoverflow.com/a/22649020
 
-    unsigned int numberOfDigits = 0;
+    int numberOfDigits = 0;
     int allowedDigits = 5;
 
     // Loops until allowedDigits is 0;
+
     do {
         numberOfDigits++;
         inputNumber /= allowedDigits;
@@ -26,63 +27,72 @@ int main() {
 
     // std::cout << "Laboration 1!" << std::endl;
     int inputNumber;
+    int reversedNumber = 0, tempNumber;
+    bool isOk;
+    /// Loop 1: Ask for input until it's correct
+    do {
+        cout << "Insert a 5 digit number" << endl;
+        cin >> inputNumber;
 
-    cout << "Insert a 5 digit number" << endl;
-    cin >> inputNumber;
+        // Input control.
+        // Source: https://elearn20.miun.se/moodle/pluginfile.php/711615/mod_folder/content/0/Laborationer/Laboration%201.pdf?forcedownload=1
+        if (cin.fail()) { // the input couldn't be cast as integer value
 
-    // Input control. Source: https://elearn20.miun.se/moodle/pluginfile.php/711615/mod_folder/content/0/Laborationer/Laboration%201.pdf?forcedownload=1
-    if (cin.fail()) { // the input couldn't be cast as integer value
+            // clear failure state
+            cin.clear();
+            // discard remaining unprocessed characters in the input stream
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            isOk = false;
+//            cout << "No alphabetic characters allowed";
 
-        // clear failure state
-        cin.clear();
-        // discard remaining unprocessed characters in the input stream
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        } else if (!isCorrectLength(inputNumber)) {
+            isOk = false;
 
-        cout << "No alphabetic characters allowed";
-
-    } else if (!isCorrectLength(inputNumber)) {
-
-        cout << "Not correct length.";
-
-    } else {
-
-        // declares after inputcontrol
-        int divisor = 1, leadingNumber, trailingNumber;
-
-
-        /* Identify the divider to be able to split the number into digits by dividing input number with
-         * the value of divisior, that increases by multiplying itself in each iteration.
-         * Simplified: Creates a "decimal number" and split
-         * Source: https://www.geeksforgeeks.org/check-number-palindrome-not-without-using-extra-space/
-         */
-        while (inputNumber / divisor >= 10) {
-
-            divisor *= 10;
-        }
-        while (inputNumber != 0) {
-
-            leadingNumber = inputNumber / divisor;
-            trailingNumber = inputNumber % 10;
-
-            // If first and last digit not the same output will be
-            if (leadingNumber != trailingNumber) {
-
-                cout << "This is not an palindrome" << endl;
-                return 0;
-            }
-
-            // Removing the controlled digits (leading and trailing) the number
-            inputNumber = (inputNumber % divisor) / 10;
-
-            // Reducing divisor by a factor
-            // of 2 as 2 digits are dropped
-            divisor = divisor / 100;
-
+//            cout << "Not correct length.";
 
         }
-        cout << "The number: " << inputNumber << "is indeed a palindrome" << endl;
 
-        return 0;
+    } while (!isOk);
 
+
+//        // declares after inputcontrol
+    int divisor = 1, leadingNumber = 0, trailingNumber = 0;
+//
+//
+//        /* Identify the divider to be able to split the number into digits by dividing input number with
+//         * the value of divisior, that increases by multiplying itself in each iteration.
+//         * Simplified: Creates a "decimal number" and split
+//         * Source: https://www.geeksforgeeks.org/check-number-palindrome-not-without-using-extra-space/
+//         */
+    while (inputNumber / divisor >= 10) {
+
+        divisor *= 10;
     }
+    tempNumber = inputNumber;
+    int countdigits = 5;
+    do {
+//
+        leadingNumber = inputNumber / divisor; // picks out first digit
+        trailingNumber = tempNumber % 10; // picks out last digit
+        reversedNumber = reversedNumber * 10 + trailingNumber; // build reverted number
+        tempNumber = tempNumber / 10; // remove digit from controlnumber
+        if (inputNumber != trailingNumber) {
+            isOk = false;//for breaking the loop
+        }
+        // Removing the controlled digits (leading and trailing) the number
+        inputNumber = (inputNumber % divisor) / 10;
+        countdigits--;
+        // Reducing divisor by a factor
+        // of 2 as 2 digits are dropped
+        divisor = divisor / 100;
+
+    } while (countdigits != 0);
+
+//
+//
+//        }
+//        cout << "The number: " << inputNumber << "is indeed a palindrome" << endl;
+
+    return 0;
+
 }
