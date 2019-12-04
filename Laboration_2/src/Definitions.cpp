2346606995b545e40ebcf6ac28833dfb9aa7657c
anbj1001 <anbj1001@student.miun.se>
2019-12-04 16:35:11 +0100

    Working on evaluation of user credentials.

--- Laboration_2/src/Definitions.cpp
+++ Laboration_2/src/Definitions.cpp
@@ -1,10 +1,12 @@
 #include "Prototypes.h"
+
 using std::string;
 using std::stringstream;
 using std::cout;
 using std::endl;
+using namespace std;
 
-string mainArgumentParser(int argc, char* argv[]) {
+string mainArgumentParser(int argc, char *argv[]) {
     if (argc < 3)
         return "fail";
     stringstream ss;
@@ -15,11 +17,22 @@ string mainArgumentParser(int argc, char* argv[]) {
 bool authenticateUser(string value) {
     const string USERNAME = "Kalle";
     const string PASSWORD = "i0J0u0j0u0J0Zys0r0{";
+
     bool authPassed = false;
     /**
      * Add needed code as stated in the lab description
      */
+    string tempPass, tempUser;
 
+    auto pos = value.find("'");
+    auto findSeperation = value.find(",");
+    int counter = 0;
+    for (int i = 0; i < value.length(); ++i) {
+
+
+        if (value[i] == '\'')//Hittar ' tecknet i stringen. Är det vad man ska göra? Tänker att man hittar infon mellan '' och kan extrahera username och password.
+            counter = i;
+    }
     return authPassed;
 }
 
