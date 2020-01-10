7eb552214a60b903648da5996873eb2847500b09
anbj1001 <anbj1001@student.miun.se>
2020-01-10 10:12:40 +0100

    subMenu() with input validation DONE. Work in progress mainMenu()

--- Laboration_3/src/main.cpp
+++ Laboration_3/src/main.cpp
@@ -1,30 +1,39 @@
 #include "Prototypes.h"
 
-bool inputValidation(int userInput);
+//TODO move function heads to Prototypes.h
+bool inputValidation();
+
+bool inputValidationMainMenu(int input);
+
+bool inputValidationSubMenu(int userInput);
+
 
 int main() {
-    int test = subMenu();
+//    int test = subMenu();
+    mainMenu();
     return 0;
 }
 
 void mainMenu() {
-
+// TODO Add code as required in submission description
     do {
-        int userInput = 0;
+        int userMenuChoice = 0;
         bool exit = false;
 
         std::cout << "FIBONACCI SEQUENCE MEASUREMENTS" << "\n" << "===============================" << std::endl;
         std::cout << "1. Run measurements" << "\n" << "2. EXIT" << std::endl;
-        std::cin >> userInput;
+        std::cin >> userMenuChoice;
 
-        if (inputValidation(userInput)) {
+        if (inputValidationMainMenu(userMenuChoice)) {
 
-            while (userInput == 1) {
-                std::cout << "Works!";
-                fibonacciTimer(30);
+            while (userMenuChoice == 1) {
 
+                size_t userInput = subMenu();
+
+                fibonacciTimer(userInput);
+                // TODO add CALL printStats() and writeToFile()
             }
-            if (userInput == 2) { exit = true; }
+            if (userMenuChoice == 2) { exit = true; }
         }
     } while (!exit);
 }
@@ -37,13 +46,32 @@ size_t subMenu() {
         cout << "Select n'th number to find:" << "\n";
         cin >> userInput;
 
-    } while (!inputValidation(userInput));
+
+    } while (!inputValidationSubMenu(userInput));
 
     return userInput;
 }
 
-bool inputValidation(int userInput) {
+bool inputValidationSubMenu(int userInput) {
 
+    if (inputValidation()) return true;
+    else {
+        std::cout << "Try again with a lower value" << std::endl;
+        return false;
+    }
+}
+
+bool inputValidationMainMenu(int userInput) {
+    if (inputValidation() && (userInput > 0 && userInput < 3)) return true;
+    else {
+        std::cout << "You can only use the given options" << "\n" << std::endl; // Error message. Helps user to correct it's input
+        return false;
+    }
+
+}
+
+bool inputValidation() {
+// WORKS
     using namespace std;
 
     if (cin.fail()) { // the input couldn't be cast as integer value
@@ -54,16 +82,10 @@ bool inputValidation(int userInput) {
         // discard remaining unprocessed characters in the input stream
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-        cout << "Use only numeric values" << "\n" << endl; // Error message. Helps user to correct it's input
+        cout << "Use only numeric values with a maximum of 2 147 483 647" << endl; // Error message. Helps user to correct it's input
 
         return false;
 
 
-    } else if (userInput < 1 || userInput > 2) {
-        cout << "You can only use the given options" << "\n" << endl; // Error message. Helps user to correct it's input
-
-        return false;
-
-    }
-    return true;
+    } else return true;
 }
