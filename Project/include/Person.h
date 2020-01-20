4747bdfcead1129cc994df3296eca0bf735bf56b
anbj1001 <anbj1001@student.miun.se>
2020-01-20 18:15:07 +0100

    commented in return 0 in main

---
+++ Project/include/Person.h
@@ -0,0 +1,8 @@
+//
+// Created by annic on 2020-01-20.
+//
+
+#ifndef DT018G_PERSON_H
+#define DT018G_PERSON_H
+
+#endif //DT018G_PERSON_H
---
+++ Project/include/constants.h
@@ -0,0 +1,8 @@
+//
+// Created by annic on 2020-01-20.
+//
+
+#ifndef DT018G_CONSTANTS_H
+#define DT018G_CONSTANTS_H
+
+#endif //DT018G_CONSTANTS_H
