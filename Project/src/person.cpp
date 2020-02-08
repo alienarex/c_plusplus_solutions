f2492cb637c0c216307b4515fe745505668b1410
anbj1001 <anbj1001@student.miun.se>
2020-02-08 19:31:54 +0100

    Added person.cpp. Does not contain any code

--- Project/src/person.cpp
+++ Project/src/person.cpp
@@ -2,4 +2,46 @@
 // Created by annic on 2020-02-08.
 //
 
+#include <string>
 #include "person.h"
+//struct Person {
+//    std::string firstname;
+//    std::string lastname;
+//    std::string signature;
+//    double height;
+//
+////    friend std::ostream &operator<<(std::ostream &os, Person const &person) {
+////        return os << person.firstname
+////                  << person.lastname
+////                  << person.signature
+////                  << person.height ;
+////    }
+//
+//};
+//
+//struct compareByFirstname {
+//    inline bool operator()(const Person &struct1, const Person &struct2) {
+//        return (struct1.firstname < struct2.firstname);
+//    }
+//};
+//
+////TODO If last name is equal sort by first name
+//struct compareByLastname {
+//    inline bool operator()(const Person &struct1, const Person &struct2) {
+//        return  struct1.lastname == struct2.lastname ? struct1.firstname < struct2.firstname : struct1.lastname < struct2.lastname;
+////        return (struct1.lastname < struct2.lastname);
+//    }
+//};
+//
+//
+//struct compareByHeight {
+//    inline bool operator()(const Person &struct1, const Person &struct2) {
+//        return (struct1.height > struct2.height);
+//    }
+//};
+//
+//struct compareBySignature {
+//    inline bool operator()(const Person &struct1, const Person &struct2) {
+//        return (struct1.signature < struct2.signature);
+//    }
+//};
